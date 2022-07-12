fb790f4d5d28846a0a67e75e0c3f01090b4b61e6
José Rodolpho <j.rodolpho29@hotmail.com>
2022-07-12 15:49:49 -0300

    fix: general error option

--- src/middlewares/errorHandlerMiddleware.ts
+++ src/middlewares/errorHandlerMiddleware.ts
@@ -1,5 +1,8 @@
 import {Response, Request, NextFunction} from "express"
 
 export async function errorHandling(error: any, req: Request, res: Response, next: NextFunction) {
-    return res.status(error.status).send(error.type)
+    if(error.status && error.type){
+        return res.status(error.status).send(error.type)
+    }
+    res.sendStatus(500)
 }
