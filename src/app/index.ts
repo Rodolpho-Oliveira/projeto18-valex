c2f36d788dc7b59cb30456a4648fac8d3ba82261
José Rodolpho <j.rodolpho29@hotmail.com>
2022-07-12 16:50:48 -0300

    fix: change port to .env

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,11 +1,13 @@
 import express,{json} from "express"
 import "express-async-errors"
+import dotenv from "dotenv"
 import cors from "cors"
 import cardRouter from "../routes/cardRouter.js"
 import rechargeRouter from "../routes/rechargeRouter.js"
 import balanceRouter from "../routes/balanceRouter.js"
 import purchaseRouter from "../routes/purchaseRouter.js"
 import { errorHandling } from "../middlewares/errorHandlerMiddleware.js"
+dotenv.config()
 
 const app = express()
 app.use(cors())
@@ -17,4 +19,4 @@ app.use(balanceRouter)
 app.use(purchaseRouter)
 app.use(errorHandling)
 
-app.listen(4000)
+app.listen(process.env.PORT || 4000)
